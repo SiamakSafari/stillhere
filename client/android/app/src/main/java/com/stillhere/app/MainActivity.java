56b9bdedeb38eafd399ba470b4c22d5661aab682
Socrates <socrates@clawd.bot>
2026-01-27 21:37:32 +0000

    Remove WidgetBridgePlugin reference from MainActivity

--- client/android/app/src/main/java/com/stillhere/app/MainActivity.java
+++ client/android/app/src/main/java/com/stillhere/app/MainActivity.java
@@ -2,14 +2,7 @@ package com.stillhere.app;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.stillhere.app.widget.WidgetBridgePlugin;
 
 public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        // Register custom plugins before super.onCreate
-        registerPlugin(WidgetBridgePlugin.class);
 
-        super.onCreate(savedInstanceState);
-    }
 }
