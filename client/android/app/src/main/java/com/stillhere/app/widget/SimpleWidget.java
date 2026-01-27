25c2e81a899d8efa0c331707bd5abdce060ce6ed
Socrates <socrates@clawd.bot>
2026-01-27 21:19:29 +0000

    Replace with minimal test widget (Java only)

---
+++ client/android/app/src/main/java/com/stillhere/app/widget/SimpleWidget.java
@@ -0,0 +1,17 @@
+package com.stillhere.app.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+import com.stillhere.app.R;
+
+public class SimpleWidget extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.simple_widget);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
